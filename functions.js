d9d81a32a34bedf6b97e63b72bd95727ad642655
Shayla White <shayladwhite1@gmail.com>
2023-08-07 08:58:31 -0700

    learning functions

---
+++ functions.js
@@ -0,0 +1,10 @@
+/* 
+
+Function:
+A function in JavaScript is like a recipe that you can use over and over again to perform a specific task.
+
+Purpose:
+Functions help you organize your code and make it easier to manage and reuse. Instead of writing the same code multiple times, you create a function and then call (use) that function whenever you need to perform that task.
+
+
+*/
